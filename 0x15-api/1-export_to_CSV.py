d1e2998356bffe3456e62fb8e7c2aed3cc6116ad
Joshua Alonge <alongejoshua@gmail.com>
2022-11-17 00:42:03 +0100

    exporting data in specific formats to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+    exporting the data requested to CSV
+"""
+if __name__ == "__main__":
+    """converts json to python dictionary
+    export to csv file
+    Format:
+        "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    """
+    import csv
+    import json
+    import requests
+    import sys
+
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # User detail
+    user_det_url = url + "users?id=" + id
+    user_detail = requests.get(user_det_url)
+    usr = json.loads(user_detail.text)
+
+    # request all todo list of the ID
+    todo_det_url = url + "todos?userId=" + id
+    todo_detail_total = requests.get(todo_det_url)
+    todo_user = json.loads(todo_detail_total.text)
+
+    # create dictionary with the right format and
+    # store each dictionaary to a list
+    fieldnames = [
+        "USER_ID", "USERNAME",
+        "TASK_COMPLETED_STATUS", "TASK_TITLE"
+        ]
+    user_info = []
+    for no in range(0, len(todo_user)):
+        info = {}
+        info["USER_ID"] = id
+        info["USERNAME"] = usr[0]["name"]
+        info["TASK_COMPLETED_STATUS"] = todo_user[no]["completed"]
+        info["TASK_TITLE"] = todo_user[no]["title"]
+        user_info.append(info)
+    print(user_info)
+
+    # writing newly created py dictionary into a csv file
+    with open('USER_ID.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(user_info)
