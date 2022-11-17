eb8df14096601f59ea9e763df411308794e92de3
Joshua Alonge <alongejoshua@gmail.com>
2022-11-17 17:34:06 +0100

    gather data from an API and displays in a specific format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,36 +4,33 @@
     accept an integer as a parameter (employee ID)
 
 """
+import json
+import requests
+import sys
 
 if __name__ == "__main__":
     """ concatinating url with query strings
         request the page
         json.loads to convert json to python dictionary
     """
-    import json
-    import requests
-    import sys
 
-    id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
 
     # User detail
-    user_det_url = url + "users?id=" + id
-    user_detail = requests.get(user_det_url)
-    usr = json.loads(user_detail.text)
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
     # request all todo list of the ID
-    todo_det_url = url + "todos?userId=" + id
-    todo_detail_total = requests.get(todo_det_url)
-    todo_user2 = json.loads(todo_detail_total.text)
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     # completed todo task
-    todo_completed_url = todo_det_url + "&completed=true"
-    todo_completed = requests.get(todo_completed_url)
-    todo_completed = json.loads(todo_completed.text)
+    todo_completed = requests.get(url + "todos",
+                                  params={"userId": user_id,
+                                          "completed": "True"}
+                                  ).json()
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(usr[0]["name"], len(todo_completed), len(todo_user2)))
+          .format(user_id["name"], len(todo_completed), len(todos)))
     # loop through to display each task title
     for task in todo_completed:
         print("\t {}".format(task["title"]))
